7c9bd352f1d82d1a6489df4fcbf8201fdcec8218
3GATO3 <106358166+Wilbert-Miau@users.noreply.github.com>
2024-03-11 00:11:27 -0600

    fix: path of test data

--- explorer_app/test_data.py
+++ explorer_app/test_data.py
@@ -1,6 +1,6 @@
 from .data_utils import reshape_debt_data
 
-sample_data_path = '../data/general_government_debt.csv'
+sample_data_path = 'data/general_government_debt.csv'
 
 transformed_data = reshape_debt_data(sample_data_path)
 print(transformed_data.tail())  
